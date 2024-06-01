5f2a35e6d958b8d7ea6ea6d2d94afe5ca99354ef
Showkat <showkat506anol@gmail.com>
2024-06-01 16:21:42 +0200

    Add a new suite

---
+++ Test Suites/Login_test_suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login_test_suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ef4eddd6-a37d-4c18-9b08-9b18ffc1c693</testSuiteGuid>
+</TestSuiteEntity>
